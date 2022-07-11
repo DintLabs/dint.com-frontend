b5c2f21bc87cc3480a0a96fa3026721e50db91b4
harshchalodiya-2002 <72147692+harshchalodiya-2002@users.noreply.github.com>
2022-07-11 12:23:54 +0530

    Update Login.js

--- src/frontend/components/Login.js
+++ src/frontend/components/Login.js
@@ -208,18 +208,13 @@ const Login = (props) => {
 
                     <button id='signup_btn' onClick={loginClicked}>Submit</button>
 
-
                     <p id="signup_line">  Not registered Yet? <Link to="/signup"> <span id='signup_here'> SignUp Here</span></Link></p>
-
                 <center>
                     <h4>or</h4>
-
                     <button onClick={googleSignin} className="authbtnsocial" style={{backgroundColor:'red'}}>Google</button>
                     <button onClick={fbSignin} className="authbtnsocial" style={{backgroundColor:'blue'}}>Facebook</button>
                     <button onClick={appleSignin} className="authbtnsocial" style={{backgroundColor:'black'}}>Iphone</button>
-
                     </center>
-
                 </div>
             </div>
             <Footer />

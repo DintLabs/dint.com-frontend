953e08cb3ea423411a96bcfdb9d4e2b77648f179
maxxpa <info@dint.com>
2022-07-24 05:14:30 -0400

    Update Events.js

--- src/frontend/components/Events.js
+++ src/frontend/components/Events.js
@@ -241,7 +241,8 @@ const Events = (props) => {
                                             <h6>End Time:  {ev.eventEndTime} </h6>
                                             <h6>Venue: {ev.venueName} </h6>
                                             <h6>Network: <DisplaynetworkLogo networkName={ev.network} />  </h6>
-                                            <h6>Tokens Required: <b>{ev.balanceRequired} </b> {ev.tokenName} <DisplaycryptoLogo /> </h6>
+                                            <h6>Token Name: {ev.tokenName} <DisplaycryptoLogo /> </h6>
+                                            <h6>Balance Required: <b> {ev.balanceRequired} </b>  </h6>
                                             <br />
                                             <ShowTicketBtn balance={userBalanceEvent} required={ev.balanceRequired} detail={ev} />
                                         </Card.Body>

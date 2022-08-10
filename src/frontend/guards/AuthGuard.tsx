9f6e3dd32901d8f09a72a597bfbdac338a14e9a1
Nikunj Maniya <nikunjmaniya8399@gmail.com>
2022-08-10 10:17:00 +0530

    auth guard issue fix

--- src/frontend/guards/AuthGuard.tsx
+++ src/frontend/guards/AuthGuard.tsx
@@ -1,6 +1,7 @@
 // import Login from 'frontend/pages/Login/Login';
-import { ReactNode, useState } from 'react';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 // hooks
 import useAuth from '../hooks/useAuth';
 
@@ -13,56 +14,57 @@ type AuthGuardProps = {
 export default function AuthGuard({ children }: AuthGuardProps) {
   const { isAuthenticated } = useAuth();
   const { pathname } = useLocation();
-  const [requestedLocation, setRequestedLocation] = useState<string | null>(null);
+  // const [requestedLocation, setRequestedLocation] = useState<string | null>(null);
   const navigate = useNavigate();
   console.log(pathname);
 
-  // if (!isAuthenticated && isInitialized) {
-  //   Swal.fire({
-  //     title: 'You are not logged in',
-  //     text: 'Click login button to Login',
-  //     icon: 'error',
-  //     showCancelButton: true,
-  //     confirmButtonColor: '#3085d6',
-  //     cancelButtonColor: '#CBC9C9',
-  //     confirmButtonText: 'Login',
-  //     cancelButtonText: 'Home'
-  //   }).then((result: any) => {
-  //     if (result.isConfirmed) {
-  //       navigate(`/auth/login`, {
-  //         state: {
-  //           redirectUrl: pathname
-  //         }
-  //       });
-  //       // return <Login />;
-  //     } else {
-  //       navigate('/', { replace: true });
-  //     }
-  //   });
+  if (!isAuthenticated) {
+    Swal.fire({
+      title: 'You are not logged in',
+      text: 'Click login button to Login',
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#CBC9C9',
+      confirmButtonText: 'Login',
+      cancelButtonText: 'Home'
+    }).then((result: any) => {
+      if (result.isConfirmed) {
+        navigate(`/auth/login`, {
+          state: {
+            redirectUrl: pathname
+          }
+        });
+        // return <Login />;
+      } else {
+        navigate('/', { replace: true });
+      }
+    });
+    return null;
 
+    // if (pathname !== requestedLocation) {
+    //   setRequestedLocation(pathname);
+    // }
+  }
+  // if (!isAuthenticated) {
+  //   console.log(pathname, isAuthenticated);
   //   // if (pathname !== requestedLocation) {
   //   //   setRequestedLocation(pathname);
   //   // }
+
+  //   navigate(`/auth/login`, {
+  //     state: {
+  //       redirectUrl: pathname
+  //     }
+  //   });
+
+  //   return null;
   // }
 
-  if (!isAuthenticated) {
-    if (pathname !== requestedLocation) {
-      setRequestedLocation(pathname);
-    }
-
-    navigate(`/auth/login`, {
-      state: {
-        redirectUrl: pathname
-      }
-    });
-
-    return null;
-  }
-
-  if (requestedLocation && pathname !== requestedLocation) {
-    setRequestedLocation(null);
-    return <Navigate to={requestedLocation} />;
-  }
+  // if (requestedLocation && pathname !== requestedLocation) {
+  //   setRequestedLocation(null);
+  //   return <Navigate to={requestedLocation} />;
+  // }
 
   return <>{children}</>;
 }

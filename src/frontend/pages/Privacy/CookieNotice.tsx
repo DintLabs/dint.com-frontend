297f982b477923acd7dd27dfca8fb8a6b57b0205
maxxpa <info@dint.com>
2022-08-22 18:35:10 -0400

    Updates

---
+++ src/frontend/pages/Privacy/CookieNotice.tsx
@@ -0,0 +1,37 @@
+import { useTheme } from '@mui/material';
+import React from 'react';
+
+const CookieNotice = () => {
+  const theme = useTheme();
+  return (
+    <div className="container mt-5 pt-5">
+      <h1 className="text-center h1 mb-5 text-white">Cookie Notice</h1>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur distinctio, voluptatem,
+        deserunt similique sed soluta quam, quidem qui dolorem aliquid autem ut odit illo.
+        Molestiae, libero laborum. Sequi, numquam atque? Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Tenetur distinctio, voluptatem, deserunt similique sed soluta quam, quidem
+        qui dolorem aliquid autem ut odit illo. Molestiae, libero laborum. Sequi, numquam atque?.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur distinctio, voluptatem,
+        deserunt similique sed soluta quam, quidem qui dolorem aliquid autem ut odit illo.
+        Molestiae, libero laborum. Sequi, numquam atque? Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Tenetur distinctio, voluptatem, deserunt similique sed soluta quam, quidem
+        qui dolorem aliquid autem ut odit illo. Molestiae, libero laborum. Sequi, numquam atque?
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur distinctio, voluptatem,
+        deserunt similique sed soluta quam, quidem qui dolorem aliquid autem ut odit illo.
+        Molestiae, libero laborum. Sequi, numquam atque? Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Tenetur distinctio, voluptatem, deserunt similique sed soluta quam, quidem
+        qui dolorem aliquid autem ut odit illo. Molestiae, libero laborum. Sequi, numquam atque?.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur distinctio, voluptatem,
+        deserunt similique sed soluta quam, quidem qui dolorem aliquid autem ut odit illo.
+        Molestiae, libero laborum. Sequi, numquam atque? Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Tenetur distinctio, voluptatem, deserunt similique sed soluta quam, quidem
+        qui dolorem aliquid autem ut odit illo. Molestiae, libero laborum. Sequi, numquam atque?
+      </p>
+    </div>
+  );
+};
+
+export default CookieNotice;
